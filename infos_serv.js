c89279d228c3821f6e7274c3801bf2ac0f388837
BuildTools <jef.roque57@gmail.com>
2025-04-08 17:26:29 +0200

    Some comments have been edited

--- infos_serv.js
+++ infos_serv.js
@@ -7,7 +7,7 @@ function createEmbed(server) {
         .setTitle('Informations sur le serveur') // en-tête de l'embed
         .setDescription('Quelques infos sur le serveur')
         .addFields(
-            { name: 'Nom du serveur', value: `${server.name}`, inline: false }, // inline = retour à la ligne pour chaque texte
+            { name: 'Nom du serveur', value: `${server.name}`, inline: false }, // false = pas de retour à la ligne | true = retour à la ligne
             { name: 'Nombre de membres', value: `${server.memberCount}`, inline: false },
             {name: 'Nombre de salons', value: `${server.channels.cache.size}`, inline: false},
             {name: 'Nombre de rôles', value: `${server.roles.cache.size}`, inline: false},
